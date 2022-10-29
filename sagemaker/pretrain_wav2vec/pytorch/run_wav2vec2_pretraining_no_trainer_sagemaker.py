f16d383f0a5493654092512edbfbc93ade3cec64
Hillary Mutisya <hillary.mutisya@LIVE.COM>
2022-10-29 13:33:15 -0700

    add wandb requirement

--- sagemaker/pretrain_wav2vec/pytorch/run_wav2vec2_pretraining_no_trainer_sagemaker.py
+++ sagemaker/pretrain_wav2vec/pytorch/run_wav2vec2_pretraining_no_trainer_sagemaker.py
@@ -381,7 +381,7 @@ def main():
 
     # Sending telemetry. Tracking the example usage helps us better allocate resources to maintain them. The
     # information sent is the one passed as arguments along with your Python/PyTorch versions.
-    #send_example_telemetry("run_wav2vec2_pretraining_no_trainer", args)
+    # send_example_telemetry("run_wav2vec2_pretraining_no_trainer", args)
 
     # Initialize the accelerator. We will let the accelerator handle device placement for us in this example.
     accelerator = Accelerator()
